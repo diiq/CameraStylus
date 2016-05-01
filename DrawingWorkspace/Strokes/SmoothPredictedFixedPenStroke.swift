a5b1585205c32defe5ab63c9319f55d6341ae2df
Sam Bleckley <sam@mutuallyhuman.com>
2016-04-30 21:52:31 -0400

    Return to simple prediction

--- DrawingWorkspace/Strokes/SmoothPredictedFixedPenStroke.swift
+++ DrawingWorkspace/Strokes/SmoothPredictedFixedPenStroke.swift
@@ -25,36 +25,15 @@ class SmoothPredictedFixedPenStroke : BaseStroke {
     renderer.catmullRom(Array(points[start..<end]), initial:  initial, final: final)
     renderer.stroke(brushSize * brushScale)
 
-    var predictedCurve: [Point];
+
     if end > 1 {
-      if end == 3 {
-        let prediction = points[end - 1] + ((points[end - 1] - points[end - 2]) * 2)
-        predictedCurve = [points[end - 2], points[end - 1], prediction]
-      } else {
-        let pVect = makePrediction(
-          penultimateVect: points[end - 2] - points[end - 3],
-          ultimateVect: points[end - 1] - points[end - 2])
-        predictedCurve = [points[end - 3], points[end - 2], points[end - 1], points[end - 1] + pVect * 2]
-      }
+      let prediction = points[end - 1] + ((points[end - 1] - points[end - 2]) * 2)
+      let predictedCurve = [points[end - 2], points[end - 1], prediction]
 
       renderer.color(Color(r: 1, g: 0, b: 0, a: 1))
       renderer.moveTo(points[end - 1])
       renderer.catmullRom(predictedCurve, initial:  false, final: final)
       renderer.stroke(brushSize * brushScale)
     }
-  }
-
-  // This takes two *vectors* -- not points, but differences between points --
-  // and produces a third which is the length of the ultimateVect but at an 
-  // angle to the ultimateVect equal to the angle between the ultimate and
-  // penultimate -- continuing the curve, as it were. Add it to the last point
-  // in the line to get the actual prediction.
-  func makePrediction(penultimateVect v: Point, ultimateVect w: Point) -> Point {
-    let dw = w.length()
-    let dv = v.length()
-    let T = acos(v.dot(w) / (dw * dv)) * 2
-    let cosT = cos(T)
-    let sinT = sin(acos(cosT))
-    return Point(x: cosT * w.x - sinT * w.y, y: sinT * w.x + cosT * w.y)
   }
 }
